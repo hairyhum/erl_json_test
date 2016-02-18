f39c4e332d98c74798fe530c6e46196cfc833323
Daniil Fedotov <dfedotov@pivotal.io>
2016-02-18 17:26:30 +0000

    RabbitMQ benchmark

--- src/erl_json_test.erl
+++ src/erl_json_test.erl
@@ -1,21 +1,29 @@
 -module(erl_json_test).
 -export([start/0]).
 -define(RESULTS_FILE, "results.csv").
--define(NUM_TESTS, 300).
+-define(NUM_TESTS, 100).
 -define(PARSERS,
-        [{"jsonx", fun jsonx:encode/1, fun jsonx:decode/1},
-         {"yawsjson2", fun json2:encode/1, fun json2:decode/1},
-         {"jiffy", fun jiffy:encode/1, fun jiffy:decode/1},
-         {"jsonerl", fun jsonerl:encode/1, fun jsonerl:decode/1},
-         {"mochijson2", fun mochijson2:encode/1, fun mochijson2:decode/1},
-         {"jsx", fun jsx:encode/1, fun jsx:decode/1}]).
+        [
+        % {"jsonx", fun jsonx:encode/1, fun(Data) -> {map, M} = jsonx:decode(Data, [{format, map}]), M end},
+         % {"yawsjson2", fun json2:encode/1, fun json2:decode/1},
+         {"jiffy", fun jiffy:encode/1, fun(Data) -> jiffy:decode(Data, [return_maps]) end},
+         % {"jsonerl", fun jsonerl:encode/1, fun jsonerl:decode/1},
+         {"mochijson2", fun(Enc) -> iolist_to_binary(mochijson2:encode(Enc)) end, fun(Data) -> mochijson2:decode(Data) end},
+         {"jsx", fun jsx:encode/1, fun(D) -> jsx:decode(D, [return_maps]) end},
+         {"jsone", fun jsone:encode/1, fun jsone:decode/1}]).
 -define(TESTFILES,
-        [{"1x", "1x.json"},
-         {"3x", "3x.json"},
-         {"9x", "9x.json"},
-         {"27x", "27x.json"},
-         {"81x", "81x.json"},
-         {"243x", "243x.json"}]).
+        [
+         % {"1x", "1x.json"},
+         % {"3x", "3x.json"},
+         % {"9x", "9x.json"},
+         % {"27x", "27x.json"},
+         % {"81x", "81x.json"},
+         % {"243x", "243x.json"},
+         {"queues", "queues.json"},
+
+         {"channels", "channels.json"},
+         {"connections", "connections.json"}
+         ]).
 
 start() ->
     JSONs = [begin
@@ -25,9 +33,10 @@ start() ->
              end
              || {Name, FileName} <- ?TESTFILES],
     _A = [ jsonx:encode(jsonx:decode(File)) || {_, File} <- JSONs],
-    _B = [ jiffy:encode(jiffy:decode(File)) || {_, File} <- JSONs],
-    _C = [ mochijson2:encode(mochijson2:decode(File)) || {_, File} <- JSONs],
-    _D = [ jsx:encode(jsx:decode(File)) || {_, File} <- JSONs],
+    _B = [ jiffy:encode(jiffy:decode(File, [return_maps])) || {_, File} <- JSONs],
+    _C = [ iolist_to_binary(mochijson2:encode(mochijson2:decode(File))) || {_, File} <- JSONs],
+    _D = [ jsx:encode(jsx:decode(File, [return_maps])) || {_, File} <- JSONs],
+    _E = [ jsone:encode(jsone:decode(File)) || {_, File} <- JSONs],
     ResultsDeep = [[begin
                         T = {ParserName, TestName, size(JSON),
                              bench(EncFun, DecFun, JSON)},
